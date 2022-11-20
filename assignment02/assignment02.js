442f598b2bd231ff21353ed44552466e20f86d12
nataliecly <nyu17@my.bcit.ca>
2022-11-20 12:58:03 -0800

    add start and end index for page size

---
+++ assignment02/assignment02.js
@@ -0,0 +1,50 @@
+
+
+var currentPage = 1;
+var pageSize = 3;
+
+setup = function () {
+  $.ajax(
+    {
+      url: "https://api.themoviedb.org/3/movie/top_rated?api_key=e876fb3647a646e4d2e48f9eb2e4f506&language=en-US&page=1",
+      type: "GET",
+      success: function (data) {
+        // console.log(data["results"]);
+        const start_index = (currentPage - 1) * pageSize;
+        const end_index = start_index + pageSize;
+
+        for (i = start_index; i < end_index; i++) {
+          $("main").append(
+            `
+            <div>
+              ${data.results[i].title}
+              <p>
+                ${data.results[i].overview}
+              </p>
+              <img 
+                src="https://image.tmdb.org/t/p/w500/${data.results[i].poster_path}"
+              >
+              <button movieBackdropImageName="${data.results[i].backdrop_path}" class="backdropBtn"> BackDrop Image </button>
+              <hr>
+            </div>
+            `
+          );
+        }
+      },
+      error: function (error) {
+        console.log(error);
+      }
+    }
+  )
+
+  $("body").on("click", ".backdropBtn", function () {
+    // console.log(`https://image.tmdb.org/t/p/w500/${$(this).attr('movieBackdropImageName')}`);
+    $("aside").html(
+      `
+        <img src="https://image.tmdb.org/t/p/w500/${$(this).attr('movieBackdropImageName')}"> 
+      `
+    )
+  })
+}
+
+$(document).ready(setup)
