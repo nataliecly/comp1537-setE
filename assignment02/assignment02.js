a48ec2af9c5feb831aa7bbf2a434f550f1525e75
nataliecly <nyu17@my.bcit.ca>
2022-11-20 15:29:59 -0800

    Add search term text box function

--- assignment02/assignment02.js
+++ assignment02/assignment02.js
@@ -1,47 +1,57 @@
 
 // GLOBAL VARIABLES
 var currentPage = 1;
-var pageSize = 3
+var pageSize;
+var term;
+
+display_page = () => {
+  $.ajax(
+      {
+        url: `https://api.themoviedb.org/3/search/movie?api_key=e876fb3647a646e4d2e48f9eb2e4f506&language=en-US&page=1&include_adult=false&query=${term}`,
+        type: "GET",
+        success: function (data) {
+          console.log(data["results"]);
+          let start_index = (currentPage - 1) * pageSize;
+          let end_index = pageSize * (currentPage - 1) + pageSize;
+
+          $("main").empty();
+          for (i = start_index; i < end_index; i++) {
+            $("main").append(
+              `
+              <div>
+                ${data.results[i].title}
+                <p>
+                  ${data.results[i].overview}
+                </p>
+                <img 
+                  src="https://image.tmdb.org/t/p/w500/${data.results[i].poster_path}"
+                >
+                <button movieBackdropImageName="${data.results[i].backdrop_path}" class="backdropBtn"> BackDrop Image </button>
+                <hr>
+              </div>
+              `
+            );
+          }
+        },
+        error: function (error) {
+          console.log(error);
+        }
+      }
+  )
+}
 
 setup = function () {
+  pageSize = $("#pageSizeMenu option:selected").val();
   $("#search-btn").click(() => {
-    $.ajax(
-    {
-      url: "https://api.themoviedb.org/3/movie/top_rated?api_key=e876fb3647a646e4d2e48f9eb2e4f506&language=en-US&page=1",
-      type: "GET",
-      success: function (data) {
-        // console.log(data["results"]);
-        const start_index = (currentPage - 1) * pageSize;
-        const end_index = pageSize * (currentPage - 1) + pageSize;
-
-        for (i = start_index; i < end_index; i++) {
-          $("main").append(
-            `
-            <div>
-              ${data.results[i].title}
-              <p>
-                ${data.results[i].overview}
-              </p>
-              <img 
-                src="https://image.tmdb.org/t/p/w500/${data.results[i].poster_path}"
-              >
-              <button movieBackdropImageName="${data.results[i].backdrop_path}" class="backdropBtn"> BackDrop Image </button>
-              <hr>
-            </div>
-            `
-          );
-        }
-      },
-      error: function (error) {
-        console.log(error);
-      }
-    }
-  )
+    term = $("#search-term").val();
+    console.log('the entered term:', term);
+    display_page()
   })
   
   $("#pageSizeMenu").change(() => {
     pageSize = $("#pageSizeMenu option:selected").val();
     console.log("pageSize", pageSize);
+    display_page()
   })
 
   $("body").on("click", ".backdropBtn", function () {
@@ -52,6 +62,20 @@ setup = function () {
       `
     )
   })
+
+  // $("#next-btn").click(() => {
+  //   currentPage += 1;
+  //   display_page()
+  // })
+
+  // $("#prev-btn").click(() => {
+  //   currentPage -= 1;
+  //   display_page()
+  // })
+
+  
+  
+
 }
 
 $(document).ready(setup)
